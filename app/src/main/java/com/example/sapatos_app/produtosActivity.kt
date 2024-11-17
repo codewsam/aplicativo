c236326132a3d07332914de461335857f7b0fb4d
EDIGLANTHIO SAMUEL ARAUJO BRANDAO <202203813285@alunos.estacio.br>
2024-11-17 18:58:50 -0300

    é os cria do cv

---
+++ app/src/main/java/com/example/sapatos_app/produtosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sapatos_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class produtosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_produtos)
+    }
+}
